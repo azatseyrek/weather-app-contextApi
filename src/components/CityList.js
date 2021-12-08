4320b25ab630cd78811244671286fee09cf8d77f
Azat Seyrek <azatseyrek@hotmail.com>
2021-12-09 02:46:09 +0300

    select dropdown menu added

--- src/components/CityList.js
+++ src/components/CityList.js
@@ -3,12 +3,26 @@ import WeatherContext from '../context/WeatherContext'
 
 
 const CityList = () => {
+    const {cityList, selectedCity, setSelectedCity} = useContext(WeatherContext)
 
-    const {cityName, setCity} = useContext(WeatherContext)
-    console.log(cityName);
+    const handleChange = (e) => {
+        e.preventDefault()
+        const selected = e.target.value
+        setSelectedCity( selected )
+    }
+
+    const keyValue = new Date().getTime()
+    console.log(selectedCity);
+
+
     return (
         <div>
-            <h1>Weather test app</h1>
+                <select onChange={handleChange} name="Cities" id="cities">
+                <option>{selectedCity}</option>
+                    {cityList.map(city => (
+                        <option value={city}>{city}</option>
+                    ))}
+                </select>
         </div>
     )
 }
