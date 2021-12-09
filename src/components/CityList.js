ad8e41a3c826f8058061d93b67610fec75b00657
Azat Seyrek <azatseyrek@hotmail.com>
2021-12-09 13:37:09 +0300

    api call

--- src/components/CityList.js
+++ src/components/CityList.js
@@ -1,30 +1,29 @@
-import React, { useContext } from 'react'
-import WeatherContext from '../context/WeatherContext'
-
+import React, { useContext } from "react";
+import WeatherContext from "../context/WeatherContext";
 
 const CityList = () => {
-    const {cityList, selectedCity, setSelectedCity} = useContext(WeatherContext)
+  const { cityList, selectedCity, setSelectedCity } =
+    useContext(WeatherContext);
 
-    const handleChange = (e) => {
-        e.preventDefault()
-        const selected = e.target.value
-        setSelectedCity( selected )
-    }
+  const handleChange = (e) => {
+    e.preventDefault();
+    const selected = e.target.value;
+    setSelectedCity(selected);
+  };
 
-    const keyValue = new Date().getTime()
-    console.log(selectedCity);
+  const keyValue = new Date().getTime();
+  console.log(selectedCity);
 
+  return (
+    <div>
+      <select onChange={handleChange} name="Cities" id="cities">
+      <option>{selectedCity}</option>
+        {cityList.map((city,) => (
+          <option value={city}>{city}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
-    return (
-        <div>
-                <select onChange={handleChange} name="Cities" id="cities">
-                <option>{selectedCity}</option>
-                    {cityList.map(city => (
-                        <option value={city}>{city}</option>
-                    ))}
-                </select>
-        </div>
-    )
-}
-
-export default CityList
+export default CityList;
